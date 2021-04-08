b41f43a56aa0b17a0947ac4baaa38f841737cf1a
Shahad Kadhim <almuhandisa.sh@gmail.com>
2021-04-08 04:08:16 +0300

    make contents private set

--- src/Cell.kt
+++ src/Cell.kt
@@ -1,6 +1,7 @@
 class Cell {
     //This is one cell of the game grid
     var contents = " "
+    private set
     fun placeMark() {
         contents = if (TicTacToe.count % 2 == 0)  "X"  else  "O"
     }
