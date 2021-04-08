69fb5c6e597b65342d2f55269883b85f0eceb6ac
Shahad Kadhim <almuhandisa.sh@gmail.com>
2021-04-08 05:25:12 +0300

    create checkInputValid instead gameMode to check both game size and game mode

--- src/TicTacToe.kt
+++ src/TicTacToe.kt
@@ -4,7 +4,6 @@ import java.lang.Exception
 
 
 fun main() {
-        val gameSize: Int
         val minimumGameSize = 1
         val maximumGameSize = 26
 
@@ -14,36 +13,24 @@ fun main() {
         println("\n\t    (1) Human vs. Computer")
         println("\n\t    (2) Computer vs. Computer")
         println("\n\tWhich mode would you like to play? (1/2): ")
-        GameSetting.user_input = readLine()
 
-        //Keep asking for an answer from the user until we get a 1 or a 2
-        gameMode(GameSetting.user_input) //gameMode() is defines below
-        println("\n\tHow large of a grid would you like to use? ")
-        println("\n\tPlease enter an integer between $minimumGameSize and $maximumGameSize: ")
-        GameSetting.user_input = readLine()
+    //Keep asking for an answer from the user until we get a 1 or a 2
+    GameSetting.gameMode=checkInputValid(2)
+    //gameMode() is defines below
+    println("\n\tHow large of a grid would you like to use? ")
+    println("\n\tPlease enter an integer between $minimumGameSize and $maximumGameSize: ")
 
-        //validate user unput for game size
-        GameSetting.valid_input = false
-        while (!GameSetting.valid_input) {
-            if (GameSetting.user_input!!.length > 0 && GameSetting.user_input!!.substring(0, 1)
-                    .matches(Regex("[1-9]")) && minimumGameSize <= GameSetting.user_input!!.toInt() && GameSetting.user_input!!.toInt() <= maximumGameSize
-            ) {
-                GameSetting.valid_input = true
-            } else {
-                println("\n\tYou must enter a number between $minimumGameSize and $maximumGameSize: ")
-                GameSetting.user_input = readLine()
-            }
-        }
+    //validate user input for game size
+    val gameSize = checkInputValid(maximumGameSize)
 
-        //issue warning for game sizes larger than 15
-        if (GameSetting.user_input!!.toInt() > 15) {
-            println("\n\t!!WARNING!!\n\t!!WARNING!!  Games large than 15 will not display correctly if console width is restricted to 80 col (neither will this message)\n\t!!WARNING!!")
-            readLine()
-        }
-        gameSize = GameSetting.user_input!!.toInt()
+    //issue warning for game sizes larger than 15
+    gameSize.takeIf { it>15 }?.let{
+        println("\n\t!!WARNING!!\n\t!!WARNING!!  Games large than 15 will not display correctly if console width is restricted to 80 col (neither will this message)\n\t!!WARNING!!")
+        readLine()
+    }
 
-        //Create a new Game instance
-        GameSetting.game = Game(gameSize)
+    //Create a new Game instance
+    GameSetting.game = Game(gameSize)
 
         //create an array of two players
         val players = arrayOfNulls<Player>(2)
@@ -93,19 +80,11 @@ fun main() {
     }
 
 
-    //validates user input and sets the game mode
-    private fun gameMode(user_input: String?) {
-        var userInput = user_input
-        GameSetting.valid_input = false
-        while (!GameSetting.valid_input) {
-            if (userInput!!.length == 1 && userInput.substring(0, 1).matches(Regex("[1-2]"))) {
-                GameSetting.valid_input = true
-            } else {
-                println("\n\tYou must enter '1' or '2' for the game mode: ")
-                userInput = readLine()
-            }
-        }
-
-        //Set user input to gameMode for use later
-        GameSetting.gameMode = userInput!!.toInt()
+    //validates user input  game mode or game size
+    private fun checkInputValid(maxValue :Int) :Int{
+        val message=if (maxValue==2)"You must enter '1' or '2' for the game mode:" else "You must enter a number between 1 and 26:"
+        do {
+            readLine()?.takeIf { it.toIntOrNull() in 1..maxValue }?.let{return  it.toInt()}
+            println("\n\t$message ")
+        }while (true)
     }
